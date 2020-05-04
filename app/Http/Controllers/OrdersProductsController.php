8a5973c97a4f5b3d506707baaaf6bd51c09727b5
samuelClo <cloarec.samuel@hotmail.fr>
2020-05-04 17:19:46 +0200

    first commit

---
+++ app/Http/Controllers/OrdersProductsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\orders_products;
+use Illuminate\Http\Request;
+
+class OrdersProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\orders_products  $orders_products
+     * @return \Illuminate\Http\Response
+     */
+    public function show(orders_products $orders_products)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\orders_products  $orders_products
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(orders_products $orders_products)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\orders_products  $orders_products
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, orders_products $orders_products)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\orders_products  $orders_products
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(orders_products $orders_products)
+    {
+        //
+    }
+}
